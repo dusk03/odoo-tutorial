a5af0f08cdf5ff0f6a6a6e03328e090530729f7e
Dang Thang <thang.dang@nifi-is.com>
2026-03-01 12:15:57 +0700

    master, demo data tutorial

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,11 +4,16 @@
     'application': True,
      'data': [
         'security/ir.model.access.csv',
+        'data/estate.property.type.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_property_views.xml',
         'views/res_users_views.xml',
         'views/estate_menus.xml',
+    ],
+    'demo': [
+        'demo/estate.property.xml',
+        'demo/estate.property.offer.xml'
     ],
 }
